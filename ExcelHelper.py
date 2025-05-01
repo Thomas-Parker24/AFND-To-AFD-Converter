be80bfc7b5ce3c866b21ea043bf82e5f0103cb79
Thomas-Parker24 <thomys65.ta@gmail.com>
2025-04-30 20:10:48 -0500

    validate_excel_file function has been updated

--- ExcelHelper.py
+++ ExcelHelper.py
@@ -10,27 +10,25 @@ def validate_excel_file(data_frame_path):
         if any_non_string_state:
             raise Exception("All States should be string values.")
 
-        zero_entrance_symbols = DataFrameToAnalyze.iloc[1:,1]
-        any_non_string_zero_input_symbol = zero_entrance_symbols[zero_entrance_symbols.apply(type) != str].any()
+        zero_entrance_symbols = DataFrameToAnalyze.iloc[1:, 1].str.split(pat=',')
 
-        if any_non_string_zero_input_symbol:
-            raise Exception("All states on input symbol 0 must be a string.")
+        for index, value in zero_entrance_symbols.items():
+            for state in value:
+                if state.isnumeric():
+                    raise Exception(f"All states on input symbol 0 must be a string, state: {state} is not valid.")
 
-        all_zero_entrance_sates_are_valid = zero_entrance_symbols.isin(states).all()
+                if not states.str.contains(state).any():
+                    raise Exception(f"All states on input symbol 0 must exists on state list, state: {state} is not valid.")
 
-        if not all_zero_entrance_sates_are_valid:
-            raise Exception("All states on input symbol 0 must exists on state list.")
+        one_entrance_symbols = DataFrameToAnalyze.iloc[1:, 2].str.split(pat=',')
 
-        one_entrance_symbols = DataFrameToAnalyze.iloc[1:,2]
-        any_non_string_one_input_symbol = one_entrance_symbols[one_entrance_symbols.apply(type) != str].any()
+        for index, value in one_entrance_symbols.items():
+            for state in value:
+                if state.isnumeric():
+                    raise Exception(f"All states on input symbol 1 must be a string, state: {state} is not valid.")
 
-        if any_non_string_one_input_symbol:
-            raise Exception("All states on input symbol 1 must be a string.")
-
-        all_one_input_states_are_valid = one_entrance_symbols.isin(states).all()
-
-        if not all_one_input_states_are_valid:
-            raise Exception("All states on input symbol 1 must exists on state list.")
+                if not states.str.contains(state).any():
+                    raise Exception(f"All states on input symbol 1 must exists on state list, state: {state} is not valid.")
 
         result_states = DataFrameToAnalyze["ACEPTA (1) / RECHAZA (0)"][1:]
 
@@ -39,15 +37,24 @@ def validate_excel_file(data_frame_path):
         if any_non_integer_result_symbol:
             raise Exception("All state results must be a integer value.")
 
-        all_result_symbols_are_valid = result_states.isin([0,1]).all()
+        all_result_symbols_are_valid = result_states.isin([0, 1]).all()
 
         if not all_result_symbols_are_valid:
             raise Exception("All state results must be a 0 or a 1 value.")
 
-        print("Excel file passed all validations!")
+        return True
 
     except Exception as e:
         print(f"Error while trying to read file. Description: {e}")
+        return False
+
+
+def is_no_deterministic(data_frame_path):
+    DataFrameToAnalyze = pd.read_excel(io=data_frame_path, sheet_name="TEMPLATE")[1:]
+    DataFrameToAnalyze.columns = ['STATES', '0', '1', 'RESULT']
+
+    print(DataFrameToAnalyze)
+    return True
 
 
 def generate_template():
